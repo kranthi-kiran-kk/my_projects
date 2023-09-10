31dfaa63dfa97b8ad366272cb4d94f567e7028a2
kranthi-kiran-kk <kranthikiranakkisetty@gmail.com>
2023-09-10 23:58:05 +0530

    Updated file_operations/file_methods.py

--- waferFaultDetection/file_operations/file_methods.py
+++ waferFaultDetection/file_operations/file_methods.py
@@ -5,108 +5,91 @@ import shutil
 
 class File_Operation:
     """
-                This class shall be used to save the model after training
-                and load the saved model for prediction.
+        This class shall be used to save the model after training
+        and load the saved model for prediction.
 
-                Written By: iNeuron Intelligence
-                Version: 1.0
-                Revisions: None
-
-                """
-    def __init__(self,file_object,logger_object):
+    """
+    def __init__(self, file_object, logger_object):
         self.file_object = file_object
         self.logger_object = logger_object
-        self.model_directory='models/'
+        self.model_directory = 'models/'
 
-    def save_model(self,model,filename):
+    def save_model(self, model, filename):
         """
             Method Name: save_model
             Description: Save the model file to directory
             Outcome: File gets saved
             On Failure: Raise Exception
 
-            Written By: iNeuron Intelligence
-            Version: 1.0
-            Revisions: None
-"""
-        self.logger_object.log(self.file_object, 'Entered the save_model method of the File_Operation class')
+        """
+        self.logger_object.log(self.file_object, "Entered the save_model method of the File_Operation class")
         try:
-            path = os.path.join(self.model_directory,filename) #create seperate directory for each cluster
-            if os.path.isdir(path): #remove previously existing models for each clusters
+            # create separate directory for each cluster
+            path = os.path.join(self.model_directory, filename)
+            # remove previously existing models for each clusters
+            if os.path.isdir(path):
+                # remove previously existing models for each clusters
                 shutil.rmtree(self.model_directory)
                 os.makedirs(path)
             else:
-                os.makedirs(path) #
-            with open(path +'/' + filename+'.sav',
-                      'wb') as f:
-                pickle.dump(model, f) # save the model to file
-            self.logger_object.log(self.file_object,
-                                   'Model File '+filename+' saved. Exited the save_model method of the Model_Finder class')
+                os.makedirs(path)
+            with open(path + '/' + filename+'.sav', 'wb') as f:
+                # save the model to file
+                pickle.dump(model, f)
+            self.logger_object.log(self.file_object, f"Model File {filename} saved. \
+                                   Exited the save_model method of the Model_Finder class")
 
-            return 'success'
         except Exception as e:
-            self.logger_object.log(self.file_object,'Exception occured in save_model method of the Model_Finder class. Exception message:  ' + str(e))
-            self.logger_object.log(self.file_object,
-                                   'Model File '+filename+' could not be saved. Exited the save_model method of the Model_Finder class')
-            raise Exception()
+            self.logger_object.log(self.file_object, f"Exception occurred in save_model method of\
+                            the Model_Finder class. Exception message: {str(e)}")
+            self.logger_object.log(self.file_object, f"Model File {filename} could not be saved. \
+                                        Exited the save_model method of the Model_Finder class")
+            raise RuntimeError
 
-    def load_model(self,filename):
+    def load_model(self, filename):
         """
-                    Method Name: load_model
-                    Description: load the model file to memory
-                    Output: The Model file loaded in memory
-                    On Failure: Raise Exception
+        Method Name: load_model
+        Description: load the model file to memory
+        Output: The Model file loaded in memory
+        On Failure: Raise Exception
 
-                    Written By: iNeuron Intelligence
-                    Version: 1.0
-                    Revisions: None
         """
-        self.logger_object.log(self.file_object, 'Entered the load_model method of the File_Operation class')
+        self.logger_object.log(self.file_object, "Entered the load_model method of the File_Operation class")
         try:
-            with open(self.model_directory + filename + '/' + filename + '.sav',
-                      'rb') as f:
-                self.logger_object.log(self.file_object,
-                                       'Model File ' + filename + ' loaded. Exited the load_model method of the Model_Finder class')
+            with open(self.model_directory + filename + '/' + filename + '.sav', 'rb') as f:
+                self.logger_object.log(self.file_object, f"Model File {filename} loaded. \
+                Exited the load_model method of the Model_Finder class")
                 return pickle.load(f)
-        except Exception as e:
-            self.logger_object.log(self.file_object,
-                                   'Exception occured in load_model method of the Model_Finder class. Exception message:  ' + str(
-                                       e))
-            self.logger_object.log(self.file_object,
-                                   'Model File ' + filename + ' could not be saved. Exited the load_model method of the Model_Finder class')
-            raise Exception()
+        except Exception as ex:
+            self.logger_object.log(self.file_object, f"Exception occurred in load_model method of the\
+                                    Model_Finder class. Exception message: {str(ex)}")
+            self.logger_object.log(self.file_object, f"Model File {filename} could not be saved. \
+            Exited the load_model method of the Model_Finder class")
+            raise RuntimeError
 
-    def find_correct_model_file(self,cluster_number):
+    def find_correct_model_file(self, cluster_number):
         """
-                            Method Name: find_correct_model_file
-                            Description: Select the correct model based on cluster number
-                            Output: The Model file
-                            On Failure: Raise Exception
+        Method Name: find_correct_model_file
+        Description: Select the correct model based on cluster number
+        Output: The Model file
+        On Failure: Raise Exception
 
-                            Written By: iNeuron Intelligence
-                            Version: 1.0
-                            Revisions: None
-                """
-        self.logger_object.log(self.file_object, 'Entered the find_correct_model_file method of the File_Operation class')
+        """
+        self.logger_object.log(self.file_object, "Entered the find_correct_model_file \
+                                            method of the File_Operation class")
+        model_name = None
         try:
-            self.cluster_number= cluster_number
-            self.folder_name=self.model_directory
-            self.list_of_model_files = []
-            self.list_of_files = os.listdir(self.folder_name)
-            for self.file in self.list_of_files:
-                try:
-                    if (self.file.index(str( self.cluster_number))!=-1):
-                        self.model_name=self.file
-                except:
-                    continue
-            self.model_name=self.model_name.split('.')[0]
+            folder_name = self.model_directory
+            list_of_files = os.listdir(folder_name)
+            for file in list_of_files:
+                if file.index(str(cluster_number)) != -1:
+                    model_name = file.split('.')[0]
             self.logger_object.log(self.file_object,
-                                   'Exited the find_correct_model_file method of the Model_Finder class.')
-            return self.model_name
+                                   "Exited the find_correct_model_file method of the Model_Finder class.")
+            return model_name
         except Exception as e:
             self.logger_object.log(self.file_object,
-                                   'Exception occured in find_correct_model_file method of the Model_Finder class. Exception message:  ' + str(
-                                       e))
-            self.logger_object.log(self.file_object,
-                                   'Exited the find_correct_model_file method of the Model_Finder class with Failure')
-            raise Exception()
+                                   f"Exception occurred in find_correct_model_file \
+                                   method of the Model_Finder class. Exception message: {str(e)}.\
+            'Exited the find_correct_model_file method of the Model_Finder class with Failure")
+            raise RuntimeError
