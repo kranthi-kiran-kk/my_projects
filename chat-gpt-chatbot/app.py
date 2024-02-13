0553b0590fde4d9d373efe9ffe5b415cf5afca92
kranthi-kiran-kk <kranthikiranakkisetty@gmail.com>
2024-02-13 12:38:30 +0530

    updated app and index files

--- chat-gpt-chatbot/app.py
+++ chat-gpt-chatbot/app.py
@@ -17,6 +17,14 @@ app.register_error_handler(404, page_not_found)
 @app.route("/", methods=["POST", "GET"])
 def index():
 
+    if request.method == "POST":
+        prompt = request.form["prompt"]
+
+        # answer = aiapi.generate_response(prompt)
+        answer = "This is where you get gpt response"
+        response = {"answer": answer}
+        return jsonify(response), 200
+
     return render_template("index.html", **locals())
 
 
